ea9ed9235dff3bdb411ad796da27089c0f12b781
Max Kueng <me@maxkueng.com>
2012-11-07 10:25:24 +0100

    soma track events

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
 var somafm = require('./lib/somafm');
 var SomaFmStation = somafm.Station;
 
-var x = new SomaFmStation('480min', '480 Minutes');
-x.nowPlaying(function (err, artist, song, album) {
-	console.log(artist, song, album);
-});
+var stations = [
+	{
+		'stationId' : 'groovesalad',
+		'stationName' : 'Groove Salad'
+	},
+	{
+		'stationId' : 'lush',
+		'stationName' : 'Lush'
+	},
+	{
+		'stationId' : 'dronezone',
+		'stationName' : 'Drone Zone'
+	},
+	{
+		'stationId' : 'sonicuniverse',
+		'stationName' : 'Sonic Universe'
+	}
+];
 
-console.log(x);
+var i, len;
+
+for (i = 0, len = stations.length; i < len; i++) {
+	(function (station) {
+		station.soma = new SomaFmStation(station.stationId, station.stationName);
+		station.soma.on('track', function (artist, song, album) {
+			console.log(station.stationName, ':', artist, ' - ', song, ' - ', album);
+		});
+
+		station.soma.start();
+	})(stations[i]);
+}
